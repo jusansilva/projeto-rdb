dd40a27d696631826969043c1313b874a719af84
Jusan Magno Pinto da Silva <jusanmagno@gmail.com>
2020-12-29 20:27:57 -0300

    configuração de porta

--- dist/adapters/envs/app-envs.js
+++ dist/adapters/envs/app-envs.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppEnvs = void 0;
 const AppEnvs = {
     port: Number(process.env.PORTA) || 3000,
-    host: process.env.HOST || "localhost"
+    host: process.env.HOST || "0.0.0.0"
 };
 exports.AppEnvs = AppEnvs;
 //# sourceMappingURL=app-envs.js.map
