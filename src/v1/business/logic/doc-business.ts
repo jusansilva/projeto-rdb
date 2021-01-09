8210dfc8e1391aa666ef0c65ce57ef2e80b9881a
K2 - Jusan Magno Pinto da Silva <k2.jusan@grupofleury.com.br>
2021-01-09 14:27:57 -0300

    atualizacao de regras

--- src/v1/business/logic/doc-business.ts
+++ src/v1/business/logic/doc-business.ts
@@ -32,11 +32,9 @@ export class DocBusiness {
       console.log("Inicio  de criação de documentos");
       console.log("Inicio de Bilhetagem")
       const bilhetagemSave = await this.getBilhetagem(dto.bilhetagem);
-      console.log(bilhetagemSave);
       console.log("Fim de Bilhetagem")
       console.log("Inicio de Gps")
       const gpsSave = await this.getGps(dto.gps);
-      console.log(gpsSave);
       console.log("Fim de Bilhetagem")
 
 
