f90e39def9b6db3e143c1d20a68a202563e8bb2c
K2 - Jusan Magno Pinto da Silva <k2.jusan@grupofleury.com.br>
2021-02-08 17:36:17 -0300

    logica de salvar dados

--- src/v1/business/logic/doc-business.ts
+++ src/v1/business/logic/doc-business.ts
@@ -40,13 +40,25 @@ export class DocBusiness {
       let bilhetagemCount = 0;
       let j = 0
       const bilhetes = fs.readFileSync(dto.bilhetagem.tempFilePath, 'utf-8').toString().split(/\n/);
-      let bisave: BilhetagemDto[] = []
+      let bisave: BilhetagemDto[] = [];
+      let bisaveCru: BilhetagemDto[] = [];
       for (const bilhete of bilhetes) {
         bilhetagemCount++;
         j++;
         let forReplace = bilhete.replace(/[""]/g, "");
         let dados = forReplace.split(',');
         if (dados[22]) {
+          bisaveCru.push({
+            carro: dados[8],
+            linha: dados[16],
+            data: new Date(dados[22] + " GMT"),
+            cartaoId: dados[23],
+            transacao: dados[24],
+            sentido: dados[25],
+            document: `${this.uuid}-${dto.bilhetagem.name}`,
+            updatedAt: new Date,
+            createdAt: new Date
+          })
           bisave.push({
             carro: dados[8],
             linha: dados[16],
@@ -58,18 +70,19 @@ export class DocBusiness {
             updatedAt: new Date,
             createdAt: new Date
           })
-          if (bilhetagemCount === 1000 || j == bilhetes.length - 1) {
-            await this.bilhetagemRepository.createMany(bisave);
+          if (bilhetagemCount === 100000 || j == bilhetes.length - 1) {
+            await this.bilhetagemRepository.createMany(bisaveCru);
             console.log(`${j} bilhetagem salvos`)
             bilhetagemCount = 0;
-            bisave = [];
+            bisaveCru = [];
           }
         }
       }
 
 
       const gps = fs.readFileSync(dto.gps.tempFilePath).toString().split(/\n/)
-      let save: GpsImportDto[] = []
+      let save: GpsImportDto[] = [];
+      let saveG: GpsImportDto[] = []
       let gpsCount = 0;
       let i = 0
       console.log(gps.length);
@@ -78,7 +91,6 @@ export class DocBusiness {
         i++;
         let gpsArray = line.split(/[\t\n]/);
         if (gpsArray[0]) {
-
           save.push({
             data_final: new Date(gpsArray[0] + " GMT"),
             AVL: gpsArray[2],
@@ -94,13 +106,28 @@ export class DocBusiness {
             createdAt: new Date
           })
 
-          if (gpsCount === 100000 || i == gps.length) {
-            await this.gpsRepository.createMany(save);
+
+          saveG.push({
+            data_final: new Date(gpsArray[0] + " GMT"),
+            AVL: gpsArray[2],
+            carro: gpsArray[3],
+            latitude: gpsArray[4],
+            longitude: gpsArray[5],
+            ponto_notavel: gpsArray[6],
+            desc_ponto_notavel: gpsArray[7],
+            linha: gpsArray[8],
+            sentido: gpsArray[9],
+            document: `${this.uuid}-${dto.gps.name}`,
+            updatedAt: new Date,
+            createdAt: new Date
+          })
+
+          if (gpsCount === 100000 || i == gps.length -1) {
+            await this.gpsRepository.createMany(saveG);
             console.log(`${i} Gps salvos`)
             gpsCount = 0;
-            save = [];
+            saveG = [];
           }
-
         }
       }
       console.log("Fim de docs")
@@ -108,32 +135,48 @@ export class DocBusiness {
       await this.realationshipRepository.drop();
       console.log("base de relação limpa")
       console.log("Inicio de Relação")
-      const bilhetagemSave = await this.bilhetagemRepository.findDocument(`${this.uuid}-${dto.bilhetagem.name}`);
-      for (const bilhetegemON of bilhetagemSave) {
-        let relacao = await this.gpsRepository.findRelacao(bilhetegemON, `${this.uuid}-${dto.gps.name}`);
-        if (relacao) {
-          console.log(`criou carro: ${bilhetegemON.carro} com AVL: ${relacao.AVL}`);
-
-          await this.realationshipRepository.create({
-            data_gps: `${this.adicionaZero(relacao.data_final.getDay())}/${this.adicionaZero(relacao.data_final.getMonth() + 1)}/${relacao.data_final.getFullYear()} ${this.adicionaZero(relacao.data_final.getHours())}:${this.adicionaZero(relacao.data_final.getMinutes())}:${this.adicionaZero(relacao.data_final.getSeconds())}`,
-            carro: bilhetegemON.carro,
-            linha: bilhetegemON.linha,
-            AVL: relacao.AVL,
-            cartaoId: bilhetegemON.cartaoId,
-            transacao: bilhetegemON.transacao,
-            sentido: bilhetegemON.sentido,
-            latitude: relacao.latitude,
-            longitude: relacao.longitude,
-            ponto_notavel: relacao.ponto_notavel,
-            desc_ponto_notavel: relacao.desc_ponto_notavel
-          });
+      let k = 0
+      console.log(bisave.length);
+      for (const bilhetegemON of bisave) {
+        k++
+        const datePlus = new Date(bilhetegemON.data);
+        datePlus.setTime(datePlus.getTime() + 20000 * 60);
+        const dateMine = new Date(bilhetegemON.data);
+        dateMine.setTime(dateMine.getTime() - 20000 * 60);
+        let relacaoSave: RelationshipDto[] = []
+       // let Element =await  this.gpsRepository.findRelacao(bilhetegemON, `${this.uuid}-${dto.gps.name}`);
+        for(const Element of save){
+        
+          if (Element.data_final > dateMine && Element.data_final < datePlus && Element.carro === bilhetegemON.carro) {
+            console.log(`criou carro: ${bilhetegemON.carro} com AVL: ${Element.AVL}`);
+            relacaoSave.push({
+              data_gps: `${this.adicionaZero(Element.data_final.getDay())}/${this.adicionaZero(Element.data_final.getMonth() + 1)}/${Element.data_final.getFullYear()} ${this.adicionaZero(Element.data_final.getHours())}:${this.adicionaZero(Element.data_final.getMinutes())}:${this.adicionaZero(Element.data_final.getSeconds())}`,
+              carro: bilhetegemON.carro,
+              linha: bilhetegemON.linha,
+              AVL: Element.AVL,
+              cartaoId: bilhetegemON.cartaoId,
+              transacao: bilhetegemON.transacao,
+              sentido: bilhetegemON.sentido,
+              latitude: Element.latitude,
+              longitude: Element.longitude,
+              ponto_notavel: Element.ponto_notavel,
+              desc_ponto_notavel: Element.desc_ponto_notavel
+            })
+            break;
+          }
         }
+
+        console.log(`${k} bilhetagem varrida`);
+        if (k === bisave.length - 1 || k === 10000) {
+          await this.realationshipRepository.createMany(relacaoSave);
+          
+        }
+
+       
+       
+
       }
-
       console.log("Fim de Relação")
-
-
-
       const name = uuid();
       console.log("Busca de Relação")
       const relationship = await this.realationshipRepository.find();
